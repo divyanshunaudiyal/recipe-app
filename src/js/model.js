f82273eb8f24643233a5145d4bbc5af63401cda2
aagyanoddy23 <div.naudiyal@gmail.com>
2023-09-17 23:28:31 +0530

    Initial Commit

---
+++ src/js/model.js
@@ -0,0 +1,33 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`);
+    }
+
+    const data = await res.json();
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
